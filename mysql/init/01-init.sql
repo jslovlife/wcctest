9b9026b2f679d5af6c2882a24c9f402e374c4501
jslovlife <jaysonseah@gmail.com>
2025-04-19 01:47:46 +0800

    Add docker script for database creation

---
+++ mysql/init/01-init.sql
@@ -0,0 +1,18 @@
+USE bodb;
+
+CREATE TABLE IF NOT EXISTS `postcodelatlng` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postcode` varchar(8) NOT NULL,
+  `latitude` decimal(10,7) NOT NULL,
+  `longitude` decimal(10,7) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `outcodepostcodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `outcode` varchar(8) NOT NULL,
+  `lat` decimal(10,7) NOT NULL,
+  `lng` decimal(10,7) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
